4f05c2e6afab4ce0f3cf57a2b586ed55b52caefa
7md4n01 <64481128+anonjokerps15@users.noreply.github.com>
2026-03-31 23:36:23 +0000

    Create celery_app.py

---
+++ celery_app.py
@@ -0,0 +1,10 @@
+from celery import Celery
+
+celery = Celery('spider_r', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+celery.conf.update(
+    task_serializer='json',
+    accept_content=['json'],
+    result_serializer='json',
+    timezone='UTC',
+    enable_utc=True,
+)
